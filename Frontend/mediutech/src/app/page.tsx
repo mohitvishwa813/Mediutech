dac93b7acdcb3ddf2b614e780f73bc395e728d29
Mohit Vishwakarma <mohitvishwa813@gmail.com>
2025-08-26 11:34:32 +0530

    remove env

--- Frontend/mediutech/src/app/page.tsx
+++ Frontend/mediutech/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
     <Link href="/harsh">
-      Go to Harshs Page
+      Go to  Page
     </Link>
     </div>
   );
